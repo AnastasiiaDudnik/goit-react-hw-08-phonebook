ff22b400d7ace57944f0f65f5c964e1352d0147d
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-15 21:50:41 +0300

    add home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from 'react';
+import { lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { fetchContacts } from 'redux/operations';
 import { Layout } from './Layout/Layout';
+
+const Home = lazy(() => import('../pages/Home'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -14,7 +17,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
-      <Route index element={<div>Home page</div>} />
+      <Route index element={<Home />} />
       <Route path="/register" element={<div>register page</div>} />
       <Route path="/login" element={<div>login page</div>} />
       <Route path="/contacts" element={<div>contacts page</div>} />
