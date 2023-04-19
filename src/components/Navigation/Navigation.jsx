98087ae02b926977782a746253baade1081d487c
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-19 20:45:06 +0300

    navigation update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,14 @@
 import { Link, Nav } from './Navigation.styled';
-// import { useAuth } from 'components/hooks/useAuth';
+import { useAuth } from 'components/hooks/useAuth';
 
 export const Navigation = () => {
-  // const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <>
       <Nav>
         <Link to="/">Home</Link>
-        <Link to="/contacts">Contacts</Link>
+        {isLoggedIn && <Link to="/contacts">Contacts</Link>}
       </Nav>
     </>
   );
