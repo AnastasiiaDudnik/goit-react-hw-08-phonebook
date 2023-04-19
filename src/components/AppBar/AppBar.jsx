1534e98d6cecdf4e39bc47f63e5c02ed993e4428
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-19 15:21:14 +0300

    app bar commented menu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,15 @@
+// import { AuthNav } from 'components/AuthNav/AuthNav';
+// import { UserMenu } from 'components/UserMenu/UserMenu';
 import { Header } from './AppBar.styled';
 import { Navigation } from 'components/Navigation/Navigation';
+// import { useAuth } from 'components/hooks/useAuth';
 
 export const AppBar = () => {
+  // const { isLoggedIn } = useAuth();
   return (
     <Header>
       <Navigation />
+      {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
     </Header>
   );
 };
---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { useAuth } from 'components/hooks/useAuth';
+import { logout } from 'redux/auth/operations';
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <p>Welcome,{user.name}</p>
+      <button type="button" onClick={() => dispatch(logout())}>
+        Log out
+      </button>
+    </div>
+  );
+};
