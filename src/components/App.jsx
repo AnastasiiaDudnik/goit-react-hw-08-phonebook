954694411f8420ab7c7f7e841fc2351dd39a1156
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-19 14:53:43 +0300

    add register page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,14 @@ import { Layout } from './Layout/Layout';
 const Home = lazy(() => import('../pages/Home'));
 const Login = lazy(() => import('../pages/Login'));
 const Contacts = lazy(() => import('../pages/Contacts'));
+const Register = lazy(() => import('../pages/Register'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/register" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contacts" element={<Contacts />} />
       </Route>
---
+++ src/pages/Register.js
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet-async';
+import { RegisterForm } from '../components/RegisterForm/RegisterForm';
+
+const Register = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Register</title>
+      </Helmet>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
