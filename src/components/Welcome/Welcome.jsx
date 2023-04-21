62d5569816a4b8953c989f587c5a85e9e9576f8a
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-21 13:30:16 +0300

    home page styles corrections

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,19 +1,17 @@
-import { Gradient, WelcomeText, Iframe } from './Welcome.styled';
+import { WelcomeText, Gif } from './Welcome.styled';
 
 export const Welcome = () => {
   return (
     <>
-      <Gradient>
-        <WelcomeText>Welcome!</WelcomeText>
-      </Gradient>
+      <WelcomeText>Welcome!</WelcomeText>
 
-      <Iframe
+      <Gif
         title="happy cat"
         src="https://giphy.com/embed/2FA9tNZBm8sO0MJj88"
         width="480"
         height="480"
         allowFullScreen
-      ></Iframe>
+      ></Gif>
     </>
   );
 };
--- src/components/Welcome/Welcome.styled.jsx
+++ src/components/Welcome/Welcome.styled.jsx
@@ -1,21 +1,10 @@
 import styled from 'styled-components';
 
-export const Gradient = styled.div`
-  //   height: 100vh;
-  //   display: flex;
-  //   place-content: center;
-  //   align-items: center;
-  //   //   width: 250px;
-  //   background-image: linear-gradient(135deg, orange 60%, cyan);
-  //   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
-  //   //   filter: blur(calc(250px / 5));
-`;
-
 export const WelcomeText = styled.h1`
   font-size: 6em;
   text-align: center;
 `;
 
-export const Iframe = styled.iframe`
+export const Gif = styled.iframe`
   border: none;
 `;
