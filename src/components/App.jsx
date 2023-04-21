9433f5faa1fa4a6a44f3c63cffcb41ca003106bc
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-21 20:04:34 +0300

    add restricted route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import PropagateLoader from 'react-spinners/PropagateLoader';
 import { Layout } from './Layout/Layout';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from './hooks/useAuth';
+import { RestrictedRoute } from './RestrictedRoute';
 
 const Home = lazy(() => import('../pages/Home'));
 const Login = lazy(() => import('../pages/Login'));
@@ -26,8 +27,16 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute component={Register} redirectTo="/contacts" />
+          }
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={Login} redirectTo="/contacts" />}
+        />
         <Route path="/contacts" element={<Contacts />} />
       </Route>
     </Routes>
