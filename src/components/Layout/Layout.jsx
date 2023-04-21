3ac8c8c4aee688843aa714c7156cb562320d2fbd
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-21 14:11:59 +0300

    add spinner

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import { Container } from './Layout.styled';
+import PropagateLoader from 'react-spinners/PropagateLoader';
+import { Container, SpinnerContainer } from './Layout.styled';
 import { AppBar } from 'components/AppBar/AppBar';
 
 export const Layout = () => {
   return (
     <Container>
       <AppBar />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <SpinnerContainer>
+            <PropagateLoader aria-label="Loading Spinner" />
+          </SpinnerContainer>
+        }
+      >
         <Outlet />
       </Suspense>
     </Container>
--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -5,3 +5,10 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: 0 16px;
 `;
+
+export const SpinnerContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
