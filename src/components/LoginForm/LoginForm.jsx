35089db2354479775a430e4e19b2aaa57520b21d
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-21 14:47:17 +0300

    login form to formik and styled

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,32 +1,67 @@
 import { useDispatch } from 'react-redux';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import { logIn } from 'redux/auth/operations';
+import {
+  LoginWrap,
+  Title,
+  Label,
+  Field,
+  MailIcon,
+  PasswordIcon,
+  ErrorMessage,
+  LoginBtn,
+} from './LoginForm.styled';
+
+const SignupSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(2, 'Password should contain more than 2 symbols')
+    .max(50, 'Too Long!')
+    .required('Field is equired*'),
+  email: Yup.string().email('Invalid email').required('Field is equired*'),
+});
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = evt => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <LoginWrap>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, actions) => {
+          dispatch(
+            logIn({
+              email: values.email,
+              password: values.password,
+            })
+          );
+          actions.resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Title>Welcome Back!</Title>
+            <Label>
+              <MailIcon />
+              <span>Email</span>
+              {errors.email && touched.email ? (
+                <ErrorMessage>{errors.email}</ErrorMessage>
+              ) : null}
+              <Field type="email" name="email" />
+            </Label>
+            <Label>
+              <PasswordIcon />
+              <span>Password</span>
+              {errors.password && touched.password ? (
+                <ErrorMessage>{errors.password}</ErrorMessage>
+              ) : null}
+              <Field type="password" name="password" />
+            </Label>
+            <LoginBtn type="submit">Login</LoginBtn>
+          </Form>
+        )}
+      </Formik>
+    </LoginWrap>
   );
 };
---
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import { BiMailSend } from 'react-icons/bi';
+import { TbPassword } from 'react-icons/tb';
+import { Field as FormikField } from 'formik';
+
+export const LoginWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+
+  background-color: #15172b;
+
+  border-radius: 20px;
+  height: 500px;
+  padding: 20px;
+  width: 320px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const Title = styled.p`
+  color: #eee;
+  font-family: sans-serif;
+  font-size: 36px;
+  font-weight: 600;
+  margin-bottom: 45px;
+`;
+
+export const Subtitle = styled.p`
+  color: #eee;
+  font-family: sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  margin-top: 10px;
+  margin-bottom: 45px;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 10px;
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const MailIcon = styled(BiMailSend)`
+  margin-right: 10px;
+  stroke-width: 1px;
+`;
+
+export const PasswordIcon = styled(TbPassword)`
+  margin-right: 10px;
+  stroke-width: 1, 5px;
+`;
+
+export const ErrorMessage = styled.div`
+  color: red;
+  font-size: 14px;
+`;
+
+export const Field = styled(FormikField)`
+  background-color: #303245;
+  border-radius: 12px;
+  border: 0;
+  color: #eee;
+  font-size: 18px;
+  height: 40px;
+  outline: 0;
+  padding: 4px 20px 0;
+  width: 280px;
+  margin-bottom: 10px;
+`;
+
+export const LoginBtn = styled.button`
+  background-color: rgb(183 0 221);
+  border-radius: 12px;
+  border: 0;
+  color: #eee;
+  cursor: pointer;
+  font-size: 18px;
+  height: 50px;
+  margin-top: 40px;
+  text-align: center;
+  width: 100%;
+
+  &:hover {
+    background-color: rgb(94 7 112);
+  }
+`;
